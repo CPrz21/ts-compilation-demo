82bc2b826f943ed224e71aa86188f5107e5f104e
CPrz21 <faselona@gmail.com>
2021-03-18 10:39:05 -0600

    05-Completing the app

--- server/server.ts
+++ server/server.ts
@@ -3,26 +3,43 @@ import * as path from "path";
 // import * as _ from "lodash";
 import { Question } from "../@types/Question";
 
-const qusestions: Question[] = [{
+const questions: Question[] = [{
   title: "How to log in?",
   content: "How do I log in",
   answerCount: 2
+},{
+  title:"Is it smart to invest in commodities?",
+  content:"My bank has recently offered a new....",
+  answerCount: 1
 }]
 
 const port: string | number = process.env.port || 1337;
 
 const app = express();
 
-app.listen(port);
-app.use(express.static("public"));
-
-console.log("Listening on port " + port );
 
 app.get("/questions", (_req, res)=>{
-  debugger;
-  res.json(qusestions);
+  res.json(questions);
 })
 
 app.get("/main.js", (_req, res)=>{
   res.sendFile(path.resolve(__dirname, "..", "client", "client.js"))
 })
+
+app.get("/new", (req, res) => {
+  const question : Question = {
+    title: req.query.title as string,
+    content: req.query.content as string
+  };
+
+  questions.push(question);
+  res.json({
+    status: "OK",
+    questions   
+  })
+});
+
+app.listen(port);
+app.use(express.static("public"));
+
+console.log("Listening on port " + port );
